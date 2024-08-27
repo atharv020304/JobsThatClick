af3fb9df4abe9fa73fe3ba37deb306495c734c32
Atharv Bapat <atharvabapat3@gmail.com>
2024-08-27 21:13:32 +0530

    TRY

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,16 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'https://jobsthatclick.onrender.com', 
+        target: 'https://jobsthatclick.onrender.com/',
         changeOrigin: true,
-        
+        // Do not use 
       },
     },
   },
-})
+});
